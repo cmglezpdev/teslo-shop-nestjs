7348823c07f9277870f88232c764bfd964e33ef1
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-23 17:10:52 -0500

    feat(Root): Add Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -30,7 +31,8 @@ import { FilesModule } from './files/files.module';
     ProductsModule,
     CommonModule,
     SeedModule,
-    FilesModule
+    FilesModule,
+    AuthModule
   ],
 })
 export class AppModule {}
