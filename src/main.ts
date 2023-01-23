1c87fa4ea14025c5d5afbed2680d358b7d362f47
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-23 13:06:21 -0500

    feat(Root): use port to run the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
@@ -12,6 +13,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
    );
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${3000}`);
 }
 bootstrap();
