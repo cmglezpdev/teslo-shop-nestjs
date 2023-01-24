6c84aaa16f1b22dc1488c3901f67e56734ecee94
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-24 00:01:52 -0500

    Fix(Auth): return id in the login and register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
       delete user.password;
       return {
         ...user,
-        token: this.getJwtToken({ email: user.email })
+        token: this.getJwtToken({ id: user.id })
       }
 
     } catch (error) {
@@ -40,7 +40,7 @@ export class AuthService {
     const { email, password } = loginUserDto;
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true }
+      select: { email: true, password: true, id: true }
     })
 
     if( !user )
@@ -51,7 +51,7 @@ export class AuthService {
 
     return {
       ...user,
-      token: this.getJwtToken({ email: user.email })
+      token: this.getJwtToken({ id: user.id })
     }
   }
 
