c5f79dae7af1f3f9e82f335a4d905f761d9b9164
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-24 12:12:28 -0500

    feat(Product/Seed): User Auth decorator to protect the endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -5,12 +5,15 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(ValidRoles.superUser, ValidRoles.admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -26,11 +29,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.superUser, ValidRoles.admin)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.superUser, ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
