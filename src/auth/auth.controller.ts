0ffaccf75c4343e71d5058204817b6eb84751b37
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-24 00:00:11 -0500

    doc(Auth): Create a private route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
@@ -15,6 +16,15 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute() {
+    return {
+      ok: true,
+      message: "Hello World Private"
+    }
   }
 
 }
