547cc2bc008a9edfda47a3c2dd5323bc2154019e
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-23 21:22:54 -0500

    feat(Auth): Add login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,11 @@ export class AuthController {
   @Post('register')
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
   }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -24,11 +24,30 @@ export class AuthService {
       await this.userRepository.save(user);
       delete user.password;
       return user;
+      // TODO: Return JWT
 
     } catch (error) {
       this.handleDbErrors(error);
     }
   }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    })
+
+    if( !user )
+      throw new UnauthorizedException('Credentials are not valid (email)');
+      
+    if( !bcrypt.compareSync(password, user.password) )
+      throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user;
+    // TODO: Return JWT
+  }
+
 
   private handleDbErrors( error: any ): never {
     switch( error.code ) {
