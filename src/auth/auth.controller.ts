49edfdae74ef31e5a02752e2b81676278ac249cb
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-24 11:36:25 -0500

    feat(Auth): Add custom Guard to check user roles to visit a link

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { IncomingHttpHeaders } from 'http';
-import { Controller, Post, Body, Get, UseGuards, Headers } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Headers, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { GetUser, RawHeaders } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -36,6 +37,18 @@ export class AuthController {
       userEmail,
       rawHeaders,
       headers
+    }
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
+      user
     }
   }
 
