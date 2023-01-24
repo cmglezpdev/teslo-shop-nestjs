ae898460bad0617dfb02d0daa1ca190cc0d5aa0c
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-23 21:46:34 -0500

    feat(Auth): Add JWT global configurations

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { User } from './entities/user.entity';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([ User ])
+    TypeOrmModule.forFeature([ User ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: ( configService: ConfigService ) => {
+        if( !configService.get('JWT_SECRET') ){
+          throw new Error('JWT_SECRET is not defined')
+        }
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    })
   ],
   exports: [TypeOrmModule]
 })
